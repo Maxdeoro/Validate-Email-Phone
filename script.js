ca395e82075a0a1f8f7259edf53cb1305f15bf4d
Maxdeoro <maxdeoro@gmail.com>
2024-11-07 23:05:19 +0300

    validate phone number

--- script.js
+++ script.js
@@ -15,3 +15,23 @@ function checkEmail() {
     document.getElementById('span1').innerText = 'Incorrect email !';
   }
 };
+
+const validatePhone = (phone) => {
+  return String(phone)
+  .toLowerCase()
+  .match(
+    // /^[\+]?[0-9]{0,3}\W?+[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
+    // /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]*$/g
+    /^[+]*[(]{0,1}[0-9]{1,3}[)]{0,1}[-\s\./0-9]{8,14}$/g 
+  );
+};
+
+function checkPhone() {
+  const phoneNumber = document.form2.phone.value;
+  
+  if(validatePhone(phoneNumber)) {
+    document.getElementById('span2').innerText = 'It s OK !';
+  } else {
+    document.getElementById('span2').innerText = 'Incorrect phonenumber !';
+  }
+};
